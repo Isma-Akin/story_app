cb39322eae9d3269f0c68f3aa9e17bb4d6c2024d
Isma-Akin <aakinsemoyin@hotmail.com>
2021-12-26 22:11:57 +0000

    refactor(data): jpg to png

--- lib/data.dart
+++ lib/data.dart
@@ -2,7 +2,7 @@ List<String> images = [
   "assets/image_04.jpg",
   "assets/image_03.jpg",
   "assets/image_02.jpg",
-  "assets/image_01.jpg",
+  "assets/image_01.png",
 ];
 
 List<String> title = [
