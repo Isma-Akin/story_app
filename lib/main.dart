cfea01278e54911d03b09067f6bcc386b3d2511b
Isma-Akin <aakinsemoyin@hotmail.com>
2021-12-26 21:15:11 +0000

    refactor(main): add allignment widget

--- lib/main.dart
+++ lib/main.dart
@@ -47,9 +47,9 @@ class _MyHomePageState extends State<MyHomePage> {
     PageController controller = PageController(initialPage: images.length - 1);
     controller.addListener(() {
       setState(() {
-        currentPage = controller.page
+        currentPage = controller.page;
       });
-    })
+    });
     return Scaffold(
         backgroundColor: Color(0xFF2d3447),
         body: SingleChildScrollView(
@@ -182,15 +182,70 @@ class CardScrollWidget extends StatelessWidget {
         var primaryCardLeft = safeWidth - widthOfPrimaryCard;
         var horizontalInset = primaryCardLeft / 2;
 
+        List<Widget> cardList = new List();
 
         for (var i = 0; i < images.length; i++) {
           var delta = i - currentPage;
+          bool isOnRight = delta > 0;
+
+          var start = padding + max(primaryCardLeft - horizontalInset * -delta * (isOnRight ? 15 : 1),
+              0.0);
 
           var cardItem = Positioned.directional(
             top: padding + verticalInset * max(-delta,0.0),
+              bottom: padding + verticalInset * max(-delta,0.0),
+              start: start,
+              textDirection: TextDirection.rtl ,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(3.0, 6.0),
+                        blurRadius: 10.0
+                      )
+                    ]
+                  ),
+                  child: AspectRatio(
+                    aspectRatio: cardAspectRatio,
+                    child: Stack(
+                      fit: StackFit.expand,
+                      children: <Widget>[
+                        Image.asset(images[i], fit: BoxFit.cover),
+                        Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                              child: Text(title[i],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 25.0,
+                                  fontFamily: "SF-Pro-Text-Regular"
+                                ),
+                              ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
 
-              textDirection: textDirection, child: child)
+                ),
+              )
+          );
+          cardList.add(cardItem);
         }
+        return Stack(
+          children: cardList,
+        );
       },
     ),);
   }
